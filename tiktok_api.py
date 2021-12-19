f02448f70b5e84ebe45ef57e0e80c6bd9d0b8ab9
Claus <renessmi2018@gmail.com>
2021-12-19 13:53:30 -0800

    Added save dir existence check in download_videos

--- tiktok_api.py
+++ tiktok_api.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from rich.traceback import install as ad_fancy_traceback
 from TikTokApi import TikTokApi
@@ -7,7 +8,7 @@ ad_fancy_traceback()
 
 
 ## you will likely need to replace this with your own see article : https://blog.devgenius.io/tiktok-api-python-41d76c67a833
-verifyFp = "verify_kxcmt49v_Mh9GhNFs_4RiC_4yVp_B1l7_X6ChWQQ5Qn5q"
+verifyFp = "verify_kxdoktdi_4f9WkyIC_IlKW_4ucX_89jY_j3EaD4Z4uA80"
 
 
 def get_video_dicts():
@@ -28,10 +29,13 @@ def filter_download_urls(video_dicts):
     return [get_download_url(video_dict) for video_dict in video_dicts]
 
 
-def download_videos(api, urls):
+def download_videos(api, urls,save_path='./downloaded_videos/'):
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
     for index, download_url in enumerate(urls):
         video_bytes = api.get_video_by_download_url(download_url)
-        with open(f"./downloaded_videos/saved_video{index}.mp4", "wb") as f:
+        # Create a save dir if there is not one
+        with open(os.path.join(save_path,f"saved_video{index}.mp4"), "wb") as f:
             f.write(video_bytes)
 
 
