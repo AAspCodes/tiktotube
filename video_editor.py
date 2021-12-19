acdf49c0d76da2e999172eb27e56ebaf8b0e220c
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2021-12-18 16:05:17 -0800

    run Black autoformat

--- video_editor.py
+++ video_editor.py
@@ -2,15 +2,16 @@
 from typing import Concatenate
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 from rich.traceback import install
+
 install()
 
 # Load myHolidays.mp4 and select the subclip 00:00:50 - 00:00:60
-clip = VideoFileClip("sample-mp4-file.mp4").subclip(10,20)
-clip2 = VideoFileClip("sample-mp4-file.mp4").subclip(15,40)
+clip = VideoFileClip("sample-mp4-file.mp4").subclip(10, 20)
+clip2 = VideoFileClip("sample-mp4-file.mp4").subclip(15, 40)
 
 
 # concatenate video clips
-video = concatenate_videoclips([clip,clip2])
+video = concatenate_videoclips([clip, clip2])
 
 # Write the result to a file (many options available !)
 video.write_videofile("new_concated_video.mp4")
