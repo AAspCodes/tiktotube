48e250e887da8e67b8519703dce671adb49e43ee
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2021-12-18 18:46:41 -0800

    Update tiktok_api.py

--- tiktok_api.py
+++ tiktok_api.py
@@ -1,8 +1,18 @@
+import json
+
 from rich.traceback import install as ad_fancy_traceback
 from TikTokApi import TikTokApi
 
 ad_fancy_traceback()
 
 verifyFp = "verify_kxcmt49v_Mh9GhNFs_4RiC_4yVp_B1l7_X6ChWQQ5Qn5q"
+
 api = TikTokApi.get_instance(custom_verifyFp=verifyFp)
-print(api.by_trending(count=1))
+trending = api.by_trending(count=1)
+
+with open("api_output.json", "w") as f:
+    f.write(json.dumps(trending))
+
+
+download_address = trending[0]["video"]["downloadAddr"]
+print(download_address)
