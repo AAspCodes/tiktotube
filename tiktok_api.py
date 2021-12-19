f09aa7c2263a8a9bb41441a51828fe685b8cb034
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2021-12-18 19:07:52 -0800

    implement basic api functionality

--- tiktok_api.py
+++ tiktok_api.py
@@ -10,9 +10,12 @@ verifyFp = "verify_kxcmt49v_Mh9GhNFs_4RiC_4yVp_B1l7_X6ChWQQ5Qn5q"
 api = TikTokApi.get_instance(custom_verifyFp=verifyFp)
 trending = api.by_trending(count=1)
 
-with open("api_output.json", "w") as f:
-    f.write(json.dumps(trending))
-
-
+# with open("api_output.json", "w") as f:
+#     f.write(json.dumps(trending))
+## parse json for download url
 download_address = trending[0]["video"]["downloadAddr"]
-print(download_address)
+## download video's bytes
+result = api.get_video_by_download_url(download_url=download_address)
+## writes bytes to file
+with open("./downloaded_videos/saved_video.mp4", "wb") as f:
+    f.write(result)
