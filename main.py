1da78ccfeea43e90d73390658706e1b43f25f6c9
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2021-12-19 16:21:46 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+
+import tiktok_api
+import video_editor
+
+if len(sys.argv) != 2:
+    print('Should have 2 args, ["main.py", "<arg>"]')
+    print(f"Instead found {len(sys.argv)} args = {sys.argv}")
+    exit(0)
+
+hashtag_source = sys.argv[1]
+
+if hashtag_source == "tiktok":
+    print("running with tikok arg")
+    # tiktok_api.tiktokrun()
+    pass
+elif hashtag_source == "google":
+    print("running with google arg")
+    # tiktok_apt.googlerun()
+    pass
+else:
+    print('arg should have "tiktok" or "google", the shell script screwed up')
+    pass
+
+## temp until api has interface built
+tiktok_api.run()
+video_editor.run()
+
+exit(0)
